2a33489e429774f166fc16388050c1c22bce31d6
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-10-10 19:06:42 +0100

    TABLE delivery_type

--- logictrl/src/database/initializeDatabase.ts
+++ logictrl/src/database/initializeDatabase.ts
@@ -3,4 +3,17 @@ import { type SQLiteDatabase } from 'expo-sqlite'
 
 export async function initializeDatabase(database: SQLiteDatabase) {
     
+    // CREATE TABLE delivery_type
+    await database.execAsync(`
+        CREATE TABLE IF NOT EXISTS delivery_type (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            requires_paid BOOLEAN,
+            requires_paid_method BOOLEAN,
+            requires_received BOOLEAN,
+            requires_received_method BOOLEAN,
+            requires_pickup_location BOOLEAN,
+            requires_delivery_location BOOLEAN
+        );
+    `)
 }
