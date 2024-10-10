989931f02fbede4314627632939b64c1a3cb1012
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-10-10 19:17:27 +0100

    Function UPDATE delivery_type

--- logictrl/src/database/useDeliveryTypeDatabase.ts
+++ logictrl/src/database/useDeliveryTypeDatabase.ts
@@ -56,5 +56,38 @@ export function useDeliveryTypeDatabase() {
         }
     }
 
-    return { create }
+    // UPDATE delivery_type
+    async function update(data: DeliveryTypeDatabase) {
+        const statment = await database.prepareAsync(`
+            UPDATE delivery_type SET
+            type = $type,
+            requires_paid = $requires_paid,
+            requires_paid_method = $requires_paid_method,
+            requires_received = $requires_received,
+            requires_received_method = $requires_received_method,
+            requires_pickup_location = $requires_pickup_location,
+            requires_delivery_location = $requires_delivery_location
+               
+            WHERE id = $id
+        `)
+
+        try {
+            await statment.executeAsync({
+                $type: data.type,
+                $requires_paid: data.requires_paid,
+                $requires_paid_method: data.requires_paid_method,
+                $requires_received: data.requires_received,
+                $requires_received_method: data.requires_received_method,
+                $requires_pickup_location: data.requires_pickup_location,
+                $requires_delivery_location: data.requires_delivery_location
+            })
+
+        } catch (error) {
+            throw error
+        } finally {
+            await statment.finalizeAsync()
+        }
+    }
+
+    return { create, update }
 }
